4f67bed4a0a009e2fa7044cebb14ac5701630e9c
asmaa kamal <asma2_kamal2009@yahoo.com>
2020-04-22 10:51:02 +0200

    Updates

---
+++ precache-manifest.b3d3da4a71abd026e7fcd9df21d8bf8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336bc17bd731702f27069e963eb9cd3c",
+    "url": "/reactjs-Profile/index.html"
+  },
+  {
+    "revision": "a498f25c52e7402648a5",
+    "url": "/reactjs-Profile/static/css/main.ce118b22.chunk.css"
+  },
+  {
+    "revision": "375c1a0a9ee8d461d856",
+    "url": "/reactjs-Profile/static/js/2.95da12b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactjs-Profile/static/js/2.95da12b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a498f25c52e7402648a5",
+    "url": "/reactjs-Profile/static/js/main.529187d1.chunk.js"
+  },
+  {
+    "revision": "6e3736e6b8d670c753e7",
+    "url": "/reactjs-Profile/static/js/runtime-main.37abc778.js"
+  }
+]);
